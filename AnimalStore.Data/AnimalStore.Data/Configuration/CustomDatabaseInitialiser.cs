d7dc9f416d12248ede7527fb4495328fcefb69e9
Joseph Egglestone <joseph.egglestone@nhs.net>
2013-08-11 22:29:59 +0100

    modified data seeding in CustomDatabaseinitialised

--- AnimalStore.Data/AnimalStore.Data/Configuration/CustomDatabaseInitialiser.cs
+++ AnimalStore.Data/AnimalStore.Data/Configuration/CustomDatabaseInitialiser.cs
@@ -13,46 +13,7 @@ namespace AnimalStore.Data.Configuration
     {
         protected override void Seed(DataContext context)
         {
- /*           #region "Add some species to database"
-
-            string[] speciesList = new string[2] {
-                "Bird",
-                "Cat"
-            };
-
-            int speciesCount = speciesList.Length-1;
-            while ((speciesCount--) != 0)
-            {
-                Species species = new Species();
-                species.Name = speciesList[speciesCount];
-
-                context.Species.Add(species);
-            }
-
-            #endregion
-
-            #region "Add some breeds to database"
-
-            string[] breedList = new string[5] {
-                "German Shepard",
-                "Rottweiler",
-                "Bulldog",
-                "Yorkshire Terrier",
-                "Great Dane"
-            };
-
-            int breedCount = breedList.Length-1;
-            while ((breedCount--) != 0)
-            {
-                Breed breed = new Breed();
-                breed.Name = breedList[breedCount];
-
-                context.Breeds.Add(breed);
-            }
-
-            #endregion
-*/
-            #region "Add some dog story data to database"
+             #region "Add some dog story data to database"
 
             Species dogSpecies = new Species { Name = "Dog"};
             Breed dalmatian = new Breed { Name = "Dalmatian", Species = dogSpecies };
