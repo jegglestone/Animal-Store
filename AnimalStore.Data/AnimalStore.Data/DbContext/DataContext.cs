d5c1105c974f976764b1dfacfe34cb2f17138941
Joseph Egglestone <joseph.egglestone@nhs.net>
2013-08-11 19:39:57 +0100

    Added entity framework support

---
+++ AnimalStore.Data/AnimalStore.Data/DbContext/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Configuration;
+using System.Linq;
+using System.Data;
+using System.Data.Entity;
+using AnimalStore.Model;
+
+namespace AnimalStore.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+        { }
+    }
+}
