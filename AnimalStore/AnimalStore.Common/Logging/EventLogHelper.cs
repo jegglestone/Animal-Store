7f3778180c74e09dc798b7340b869d44c8d102f2
Joseph Egglestone <joseph.egglestone@nhs.net>
2013-09-14 10:30:32 +0100

    Error handling for EventLogHelper initialisation

--- AnimalStore/AnimalStore.Common/Logging/EventLogHelper.cs
+++ AnimalStore/AnimalStore.Common/Logging/EventLogHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Security;
 
 namespace AnimalStore.Common.Logging
 {
@@ -8,14 +9,24 @@ namespace AnimalStore.Common.Logging
        {
            const string eventlogName = "AnimalStore.Events";
 
-           if (!EventLog.SourceExists(eventlogName))
+           try
            {
-               EventLog.CreateEventSource(eventlogName, eventlogName);
+               if (!EventLog.SourceExists(eventlogName))
+               {
+                   EventLog.CreateEventSource(eventlogName, eventlogName);
 
-               var eventLog = new EventLog { Source = eventlogName, Log = eventlogName };
-               eventLog.Source = eventlogName;
-               eventLog.WriteEntry("The " + eventlogName + " was successfully created.", 
-                                   EventLogEntryType.Information);
+                   var eventLog = new EventLog { Source = eventlogName, Log = eventlogName };
+                   eventLog.Source = eventlogName;
+                   eventLog.WriteEntry("The " + eventlogName + " was successfully created.",
+                                       EventLogEntryType.Information);
+               }
+           }
+           catch(SecurityException)
+           {
+               string user = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+               string eventLogRegistryKey ="HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Services\\EventLog\\Security";
+               string message ="In order to use logging, the Event logs must be accessible. You must grant " + user + "read permissions on the registry key " + eventLogRegistryKey;
+               throw new SecurityException(message);
            }
        }
     }
