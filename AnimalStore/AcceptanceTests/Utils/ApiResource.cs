8d4ec0f304aa20a9a237ae9d5d4b3534ed01c64d
Joseph Egglestone <joseph.egglestone@nhs.net>
2013-09-27 17:47:30 +0100

    Further Acceptance test infrastructure

---
+++ AnimalStore/AcceptanceTests/Utils/ApiResource.cs
@@ -0,0 +1,17 @@
+﻿using System.Configuration;
+
+namespace AcceptanceTests.Utils
+{
+    public class ApiResource
+    {
+        public static string GetResourceUrl(string pageName)
+        {
+            return GetSiteUrl() + pageName;
+        }
+
+        private static string GetSiteUrl()
+        {
+            return ConfigurationManager.AppSettings["SiteUrl"];
+        }
+    }
+}
---
+++ AnimalStore/AnimalStore.Common/Constants/MediaTypeFormats.cs
@@ -0,0 +1,16 @@
+﻿namespace AnimalStore.Common.Constants
+{
+    public static class MediaTypeFormats
+    {
+        public static class Keys
+        {
+            public const string FORMATQUERYSTRINGKEY = "format";
+        }
+
+        public enum Values
+        {
+            JSON,
+            XML
+        }
+    }
+}
