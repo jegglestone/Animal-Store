08814bce6d7cf33c77ec1bb057ed42514692d7ef
Joseph Egglestone <joseph.egglestone@nhs.net>
2014-03-07 16:53:54 +0000

    Removed obsolete comments

--- AnimalStore/AnimalStore.Web.API/Controllers/DogsController.cs
+++ AnimalStore/AnimalStore.Web.API/Controllers/DogsController.cs
@@ -48,9 +48,9 @@ namespace AnimalStore.Web.API.Controllers
 
         // TODO: Acceptance test all these outcomes
         // GET api/dogs?breedid=1&page=1&pagesize=100&placeId=1&format=json
-        // GET api/dogs/breed?breedid=67&page=1&pagesize=30&format=json  -where is breed? No breed when appending?
+        // GET api/dogs/breed?breedid=67&page=1&pagesize=30&format=json
         // GET api/dogs/breed?breedid=7&page=1&pagesize=30&format=json
-        // GET api/dogs?breedid=4&page=1&pagesize=30&placeid=12472&format=json  -no breed when appending
+        // GET api/dogs?breedid=4&page=1&pagesize=30&placeid=12472&format=json
         [HttpGet]
         public PageableResults<Dog> GetPaged(int breedId, int page, int pageSize, string sortBy = null, int placeId = 0)
         {
